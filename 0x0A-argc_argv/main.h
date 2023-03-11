131f54de84c06e914a748fb7906ed1a3a1d2294e
zinou08 <zinebbht08@gmail.com>
2023-03-10 17:48:47 -0800

    argc argv

---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,8 @@
+#ifndef MAIN_H 
+#define MAIN_H 
+int _putchar(char c); 
+int _atoi(char *s); 
+int main(int argc __attribute__((unused)), char *argv[]); 
+int main(int argc, char *argv[]); 
+int check_num(char *str);
+#endif 
